e1f517a4a817bc8116438e58b9ee553440aa8564
Xientra <articbuisness@gmail.com>
2023-02-01 18:25:59 +0000

    fix: optional parameters not actually optional

--- src/logger.py
+++ src/logger.py
@@ -10,25 +10,25 @@ logging.basicConfig(
 
 class Logger:
     @staticmethod
-    def info(message: str, silent: Optional[bool]):
+    def info(message: str, silent: Optional[bool] = None):
         logging.info(message)
         if not silent:
             print(f"[INFO] {message}")
 
     @staticmethod
-    def error(message: str, silent: Optional[bool]):
+    def error(message: str, silent: Optional[bool] = None):
         logging.error(message)
         if not silent:
             print(f"[ERROR] {message}")
 
     @staticmethod
-    def warn(message: str, silent: Optional[bool]):
+    def warn(message: str, silent: Optional[bool] = None):
         logging.warn(message)
         if not silent:
             print(f"[WARN] {message}")
 
     @staticmethod
-    def exception(message: str, silent: Optional[bool]):
+    def exception(message: str, silent: Optional[bool] = None):
         logging.exception(message)
         if not silent:
             print(f"[EXCEPTION] {message}")
