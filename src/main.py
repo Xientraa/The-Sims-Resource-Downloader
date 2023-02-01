19c1eebfe58355017bdb5a4a240061ea29fb3a3d
Xientra <articbuisness@gmail.com>
2023-02-01 15:26:40 +0000

    feat: add multiprocessing

--- src/main.py
+++ src/main.py
@@ -1,22 +1,38 @@
 from TSRUrl import TSRUrl
 from TSRDownload import TSRDownload
 from exceptions import *
+from multiprocessing import Pool
 import clipboard, time
 
 if __name__ == "__main__":
     lastPastedText = ""
+    runningDownloads = []
     while True:
         pastedText = clipboard.paste()
         if lastPastedText != pastedText:
             lastPastedText = pastedText
-            try:
-                url = TSRUrl(pastedText)
-                print(f"[INFO] Found valid url in clipboard: {url.url}")
+            if lastPastedText in runningDownloads:
+                print(f"[INFO] Url is already being downloaded: {pastedText}")
+                continue
+
+            def processTarget(url: TSRUrl):
                 print(f"[INFO] Getting 'tsrdlticket' cookie for: {url.url}")
                 downloader = TSRDownload(url)
                 print(f"[INFO] Starting download for: {url.url}")
                 if downloader.download():
                     print(f"[INFO] Completed download for: {url.url}")
+
+                return url
+
+            def callback(url: TSRUrl):
+                runningDownloads.remove(url.url)
+
+            try:
+                url = TSRUrl(pastedText)
+                runningDownloads.append(url.url)
+                print(f"[INFO] Found valid url in clipboard: {url.url}")
+                pool = Pool(1)
+                pool.apply_async(processTarget, args=[url], callback=callback)
             except InvalidURL:
                 pass
 
