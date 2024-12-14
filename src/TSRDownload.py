b60da1b3c7d0c298a1e5d6c9d275150d9f674914
Farahat <67980665+iseeface@users.noreply.github.com>
2024-12-14 20:17:35 +0700

    feat: add progress download bar
    my mistake was stacking the download function, and I have fixed it

--- src/TSRDownload.py
+++ src/TSRDownload.py
@@ -18,81 +18,6 @@ class TSRDownload:
 
     @classmethod
     def download(self, downloadPath: str) -> str:
-        logger.info(f"Starting download for: {self.url.url}")
-        timeToSleep = 15000 - (time.time() * 1000 - self.ticketInitializedTime)
-        if timeToSleep > 0:
-            time.sleep(timeToSleep / 1000)
-
-        downloadUrl = self.__getDownloadUrl()
-        logger.debug(f"Got downloadUrl: {downloadUrl}")
-        fileName = self.__getFileName(downloadUrl)
-        logger.debug(f"Got fileName: {fileName}")
-
-        startingBytes = (
-            os.path.getsize(f"{downloadPath}/{fileName}.part")
-            if os.path.exists(f"{downloadPath}/{fileName}.part")
-            else 0
-        )
-        logger.debug(f"Got startingBytes: {startingBytes}")
-        request = self.session.get(
-            downloadUrl,
-            stream=True,
-            headers={"Range": f"bytes={startingBytes}-"},
-        )
-        logger.debug(f"Request status is: {request.status_code}")
-        file = open(f"{downloadPath}/{fileName}.part", "wb")
-
-        for index, chunk in enumerate(request.iter_content(1024 * 128)):
-            logger.debug(f"Downloading chunk #{index} of {downloadUrl}")
-            file.write(chunk)
-        file.close()
-        logger.debug(f"Removing .part from file name: {fileName}")
-        if os.path.exists(f"{downloadPath}/{fileName}"):
-            logger.debug(f"{downloadPath}/{fileName} Already exists! Replacing file")
-            os.replace(f"{downloadPath}/{fileName}.part", f"{downloadPath}/{fileName}")
-        else:
-            logger.debug(f"{downloadPath}/{fileName} doesn't exist! Renaming file")
-            os.rename(
-                f"{downloadPath}/{fileName}.part",
-                f"{downloadPath}/{fileName}",
-            )
-        return fileName
-
-    @classmethod
-    def __getFileName(self, downloadUrl: str) -> str:
-        return re.search(
-            '(?<=filename=").+(?=")',
-            requests.get(downloadUrl, stream=True).headers["Content-Disposition"],
-        )[0]
-
-    @classmethod
-    def __getDownloadUrl(self) -> str:
-        response = self.session.get(
-            f"https://www.thesimsresource.com/ajax.php?c=downloads&a=getdownloadurl&ajax=1&itemid={self.url.itemId}&mid=0&lk=0",
-            cookies=self.session.cookies,
-        )
-        responseJSON = response.json()
-
-        if response.status_code == 200:
-            if responseJSON["error"] == "":
-                return responseJSON["url"]
-            elif responseJSON["error"] == "Invalid download ticket":
-                raise InvalidDownloadTicket(response.url, self.session.cookies)
-        else:
-            raise requests.exceptions.HTTPError(response)
-
-    @classmethod
-    def __getTSRDLTicketCookie(self) -> str:
-        logger.info(f"Getting 'tsrdlticket' cookie for: {self.url.url}")
-        response = self.session.get(
-            f"https://www.thesimsresource.com/ajax.php?c=downloads&a=initDownload&itemid={self.url.itemId}&format=zip"
-        )
-        self.session.get(self.url.downloadUrl)
-        self.ticketInitializedTime = time.time() * 1000
-        return response.cookies.get("tsrdlticket")
-
-    @classmethod
-    def download(self, downloadPath: str) -> str:
         logger.info(f"\n\n=== Starting download for: {self.url.url} ===")
         timeToSleep = 15000 - (time.time() * 1000 - self.ticketInitializedTime)
         if timeToSleep > 0:
@@ -138,3 +63,36 @@ class TSRDownload:
         
         logger.info(f"\n=== Download completed: {fileName} ===\n")
         return fileName
+
+    @classmethod
+    def __getFileName(self, downloadUrl: str) -> str:
+        return re.search(
+            '(?<=filename=").+(?=")',
+            requests.get(downloadUrl, stream=True).headers["Content-Disposition"],
+        )[0]
+
+    @classmethod
+    def __getDownloadUrl(self) -> str:
+        response = self.session.get(
+            f"https://www.thesimsresource.com/ajax.php?c=downloads&a=getdownloadurl&ajax=1&itemid={self.url.itemId}&mid=0&lk=0",
+            cookies=self.session.cookies,
+        )
+        responseJSON = response.json()
+
+        if response.status_code == 200:
+            if responseJSON["error"] == "":
+                return responseJSON["url"]
+            elif responseJSON["error"] == "Invalid download ticket":
+                raise InvalidDownloadTicket(response.url, self.session.cookies)
+        else:
+            raise requests.exceptions.HTTPError(response)
+
+    @classmethod
+    def __getTSRDLTicketCookie(self) -> str:
+        logger.info(f"Getting 'tsrdlticket' cookie for: {self.url.url}")
+        response = self.session.get(
+            f"https://www.thesimsresource.com/ajax.php?c=downloads&a=initDownload&itemid={self.url.itemId}&format=zip"
+        )
+        self.session.get(self.url.downloadUrl)
+        self.ticketInitializedTime = time.time() * 1000
+        return response.cookies.get("tsrdlticket")
