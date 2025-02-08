52daab2657fb9ac0fdae2ff71abe275dbabd6430
nidgithm <88002702+nidgithm@users.noreply.github.com>
2025-02-08 19:13:57 +0100

    Add .bat files for easy setup and starting.

---
+++ setup.bat
@@ -0,0 +1,4 @@
+del src\session
+python -m venv venv
+venv\Scripts\pip.exe install -r requirements.txt
+venv\Scripts\python.exe src\main.py
---
+++ start.bat
@@ -0,0 +1,2 @@
+del src\session
+venv\Scripts\python.exe src\main.py
